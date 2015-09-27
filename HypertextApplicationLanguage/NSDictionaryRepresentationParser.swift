359d22dd19f08d505eee6b185a7ee51d6f6c4ed0
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-09-27 02:54:01 +0100

    Make parse and render methods static

--- HypertextApplicationLanguage/NSDictionaryRepresentationParser.swift
+++ HypertextApplicationLanguage/NSDictionaryRepresentationParser.swift
@@ -33,7 +33,7 @@ public class NSDictionaryRepresentationParser {
 
   /// Parses a dictionary, loading the given representation with its
   /// hypertext-application-language content.
-  public func parse(representation: Representation, object: NSDictionary) {
+  public static func parse(representation: Representation, object: NSDictionary) {
     // Takes compact URI pairs from the links. Looks for the `curies`
     // sub-dictionary within `_links` root-level dictionary. Every CURIE is a
     // dictionary with a name and a hypertext reference.
